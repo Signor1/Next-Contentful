33303d7a2c14fecd6f91393a5102e8a7713eae22
signor1 <emmanuelomemgboji@gmail.com>
2023-11-28 09:20:45 +0100

    chore: integrating contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import RecipeCard from "@/components/RecipeCard";
 import { createClient } from "contentful";
 
 export async function getStaticProps() {
@@ -5,8 +6,33 @@ export async function getStaticProps() {
     space: process.env.NEXT_CONTENTFUL_SPACE_ID,
     accessToken: process.env.NEXT_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
   });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
 }
 
-export default function Recipes() {
-  return <div>Recipe Lists</div>;
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+
+      {/* styles */}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    </div>
+  );
 }
--- src/pages/recipes/[slug].js
+++ src/pages/recipes/[slug].js
@@ -1,3 +1,65 @@
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+import { createClient } from "contentful";
+import Image from "next/image";
+
+const client = createClient({
+  space: process.env.NEXT_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields;
+
+  return (
+    <div>
+      <div className="banner">
+        <Image
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+          alt="featured image"
+        />
+        <h2>{title}</h2>
+      </div>
+      <div className="info">
+        <p>Takes about {cookingTime} mins to cook</p>
+        <h3>Ingredients:</h3>
+        {ingredients.map((ing) => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+      <div className="mathod">
+        <h3>Method</h3>
+        <div></div>
+      </div>
+    </div>
+  );
 }
