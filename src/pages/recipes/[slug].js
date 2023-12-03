76f3a68efcc2712ea353ba768e25591401161d61
signor1 <emmanuelomemgboji@gmail.com>
2023-12-03 15:19:30 +0100

    chore: updated

--- src/pages/recipes/[slug].js
+++ src/pages/recipes/[slug].js
@@ -1,3 +1,4 @@
+import Skeleton from "@/components/Skeleton";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { createClient } from "contentful";
 import Image from "next/image";
@@ -18,7 +19,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -35,6 +36,9 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
+  //for awaiting next to generate a new page for new updates
+  if (!recipe) return <Skeleton />;
+
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
 
